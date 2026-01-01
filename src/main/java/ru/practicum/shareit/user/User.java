da9cc0776a9443f90047867d9159c698e054d7ac
Anton Riabushkin <riabushkinanton@yandex.ru>
2026-01-01 12:31:52 +0700

    feat: создать класс User.

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.user;
 
-/**
- * TODO Sprint add-controllers.
- */
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Builder(toBuilder = true)
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = {"id"})
 public class User {
+    Long id;
+    String name;
+    @NotBlank(message = "Электронный адрес пользователя не может быть пустым.")
+    @Email(message = "Электронный адрес пользователя должен содержать символ @ и быть корректным.")
+    String email;
 }
