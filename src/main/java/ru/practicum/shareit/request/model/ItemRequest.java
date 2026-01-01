68f276ab328ecf4e09cef8c91be3b332e20104e0
Anton Riabushkin <riabushkinanton@yandex.ru>
2026-01-01 13:29:04 +0700

    feat: создать класс ItemRequest.

--- src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/model/ItemRequest.java
@@ -1,7 +1,25 @@
 package ru.practicum.shareit.request.model;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.*;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
+@Builder(toBuilder = true)
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode(of = {"id"})
 public class ItemRequest {
+    Long id;
+    String description;
+    @NotNull
+    @Valid
+    User requestor;
+    @NotNull
+    @PastOrPresent
+    LocalDateTime created;
 }
