6536f09dba6d5741e2de0d95a2e2328a74b9f412
Anton Ryabushkin <riabushkinanton@yandex.ru>
2026-01-06 22:10:40 +0300

    feat: добавить обработку исключений контроллеров.

--- src/main/java/ru/practicum/shareit/config/GlobalExceptionHandler.java
+++ src/main/java/ru/practicum/shareit/config/GlobalExceptionHandler.java
@@ -3,10 +3,13 @@ package ru.practicum.shareit.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 import ru.practicum.shareit.exceptions.EmailAlreadyUsedException;
 import ru.practicum.shareit.exceptions.ItemDontBelongToUserException;
 import ru.practicum.shareit.exceptions.NotFoundException;
@@ -80,6 +83,49 @@ public class GlobalExceptionHandler {
 
         ErrorResponse body = ErrorResponse.builder()
                 .errorMessage(message)
+                .errorCode(HttpStatus.BAD_REQUEST.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(body);
+    }
+
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse> handleWrongPath(NoResourceFoundException ex) {
+        String errorMessage = "Ресурс по указанному пути не найден.";
+        String logMessage = String.format("Получен запрос на несуществующий путь %s.", ex.getResourcePath());
+        log.warn(logMessage);
+
+        ErrorResponse body = ErrorResponse.builder()
+                .errorMessage(errorMessage)
+                .errorCode(HttpStatus.NOT_FOUND.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(body);
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse> handleWrongRequestMethod(HttpRequestMethodNotSupportedException ex) {
+        String errorMessage = String.format("Метод %s не поддерживается.", ex.getMethod());
+        String logMessage = String.format("Получен запрос с нереализованным методом %s.", ex.getMethod());
+        log.warn(logMessage);
+
+        ErrorResponse body = ErrorResponse.builder()
+                .errorMessage(errorMessage)
+                .errorCode(HttpStatus.METHOD_NOT_ALLOWED.value())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(body);
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse> handleTypeMismatch(MethodArgumentTypeMismatchException ex) {
+        String name = ex.getName();
+        String value = String.valueOf(ex.getValue());
+        String errorMessage = String.format("Неверный формат параметра запроса '%s': '%s'. Ожидается число.", name, value);
+        log.warn(errorMessage);
+
+        ErrorResponse body = ErrorResponse.builder()
+                .errorMessage(errorMessage)
                 .errorCode(HttpStatus.BAD_REQUEST.value())
                 .build();
 
