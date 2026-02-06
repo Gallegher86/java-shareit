d198dc28b451df17e756ca426ea3d021c38633ef
Anton Ryabushkin <riabushkinanton@yandex.ru>
2026-02-06 18:01:41 +0300

    fix: правки стиля.

--- server/src/main/java/ru/practicum/shareit/config/GlobalExceptionHandler.java
+++ server/src/main/java/ru/practicum/shareit/config/GlobalExceptionHandler.java
@@ -4,15 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingRequestHeaderException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.resource.NoResourceFoundException;
 import ru.practicum.shareit.exceptions.*;
-
-import java.util.List;
 
 @Slf4j
 @RestControllerAdvice
